8acbc56a4a489a3fec20c67e261f84e71003cf7b
BorjaG90 <borjagete90gmail.com>
2017-04-25 14:06:53 +0200

    Developing svg of shotchart #62

--- public/javascripts/throwExercise.js
+++ public/javascripts/throwExercise.js
@@ -2,7 +2,7 @@
   * Created by Borja Gete on 24/04/17.
   */
 function mostrar(){
-document.getElementById('throw-exe').style.display = 'block';}
+document.getElementById('shot-chart').style.display = 'block';}
 
 function ocultar(){
-document.getElementById('throw-exe').style.display = 'none';}
+document.getElementById('shot-chart').style.display = 'none';}
