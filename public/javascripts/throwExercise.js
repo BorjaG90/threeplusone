5737fa267ec1b9bee7079984475894241461529f
BorjaG90 <borjagete90@gmail.com>
2017-04-26 12:00:04 +0200

    Added inputs to shotchart #62

--- public/javascripts/throwExercise.js
+++ public/javascripts/throwExercise.js
@@ -6,3 +6,16 @@ document.getElementById('shot-chart').style.display = 'block';}
 
 function ocultar(){
 document.getElementById('shot-chart').style.display = 'none';}
+
+function calculatePercentage(idMade,idAtt,min,max,idSvg){
+    var made = document.getElementById(idMade).value;
+    var att = document.getElementById(idAtt).value;
+    var r = (made / att) * 100;
+     if(r <= min && r < 101){
+        document.getElementById(idSvg).className.baseVal = "zone bad-fg";
+    }else if(r>= max && r < 101){
+        document.getElementById(idSvg).className.baseVal = "zone good-fg";
+    }else{
+        document.getElementById(idSvg).className.baseVal = "zone neutral-fg";
+    }
+}
